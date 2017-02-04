4bdb56c09abe3fd4179f392acac2ad070fd3884b
zwakang85 <zwakang85@gmail.com>
2017-02-05 05:32:31 +0700

    Update blogku.js

--- blogku.js
+++ blogku.js
@@ -4,8 +4,9 @@ function setPost(){
 for (i=0;i<numItems;i++){
    var imPost= $('.homes:eq('+i+')').find('img').attr('src');
    var altPost= $('.homes:eq('+i+')').find('img').attr('alt');
+   var linkPost=$('.homes:eq('+i+')').find('post-title a').attr('href');
    $('.homes:eq('+i+')').html('');
-   $('.homes:eq('+i+')').html('<img src="'+imPost+'" alt="'+altPost+'" class="img-responsive"/>');
+   $('.homes:eq('+i+')').html('<a href="'+linkPost+'"><img src="'+imPost+'" alt="'+altPost+'" class="img-responsive"/></a>');
    $('.homes:eq('+i+')').addClass( "grid-item" );
 }
 }

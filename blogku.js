496137b1a053ab3757332761294e2d68956f7ca7
zwakang85 <zwakang85@gmail.com>
2017-02-05 04:18:05 +0700

    Update blogku.js

--- blogku.js
+++ blogku.js
@@ -1,7 +1,9 @@
 var dataPostM=[];
 var numItems = $('.homes').length;
-var imPost= $('.homes').find('img').attr('src');
-var altPost= $('.homes').find('img').attr('alt');
+function setPost(){
 for (i=0;i<numItems;i++){
+   var imPost= $('.homes:eq('+i+')').find('img').attr('src');
+   var altPost= $('.homes:eq('+i+')').find('img').attr('alt');
    $('.homes:eq('+i+')').html('');
+}
 }
